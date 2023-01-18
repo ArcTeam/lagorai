a95ca48c790f6eaee207f69784962fd48e49b220
Giuseppe Naponiello <beppenapo@arc-team.com>
2023-01-18 15:33:48 +0100

    fix manifest

--- js/init.js
+++ js/init.js
@@ -9,16 +9,31 @@ if ('serviceWorker' in navigator) {
   });
 }
 let deferredPrompt;
-window.addEventListener('beforeinstallprompt', (e) => { deferredPrompt = e; });
 const installApp = document.getElementById('installApp');
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  installApp.style.display = 'block';
+});
 installApp.addEventListener('click', async () => {
-    if (deferredPrompt !== null) {
-        deferredPrompt.prompt();
-        const { outcome } = await deferredPrompt.userChoice;
-        if (outcome === 'accepted') {
-            deferredPrompt = null;
-        }
+  installApp.style.display = 'none';
+  deferredPrompt.prompt();
+  deferredPrompt.userChoice
+  .then((choiceResult) => {
+    if (choiceResult.outcome === 'accepted') {
+      console.log('User accepted the A2HS prompt');
+    } else {
+      console.log('User dismissed the A2HS prompt');
     }
+    deferredPrompt = null;
+  });
+  // if (deferredPrompt !== null) {
+  //   deferredPrompt.prompt();
+  //   const { outcome } = await deferredPrompt.userChoice;
+  //   if (outcome === 'accepted') {
+  //     deferredPrompt = null;
+  //   }
+  // }
 });
 /////////// MAPPE ///////////////
 let map;
